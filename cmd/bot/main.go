3747b72399c1b16bd872ebd3c7900052aff6973a
Trang Nguyen <minhtrang.2106@gmail.com>
2025-10-22 13:00:05 +0700

    Task 1: Set up Clean Architecture project structure

---
+++ cmd/bot/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+	"log"
+)
+
+func main() {
+	ctx := context.Background()
+
+	log.Println("Starting Slack Translation Bot...")
+	// TODO: Initialize dependencies and start the server
+}
